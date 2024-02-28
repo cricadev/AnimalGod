81271970e5cfa4e671e640555927cb693e631464
CristianN998 <cristiancasallas998@gmail.com>
2024-02-28 12:40:17 -0500

    Refactor form layout for responsiveness

--- pages/form.vue
+++ pages/form.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overflow-hidden p-5 bg-form">
     <form @submit.prevent="handleFormSubmition"
-      class="flex flex-col justify-between h-[80vh] gap-4 w-full bg-Bg relative overflow-hidden rounded-lg">
+      class="flex flex-col justify-between h-[80vh] gap-4 bg-Bg relative overflow-hidden rounded-lg max-w-4xl mx-auto">
       <TransitionGroup name="list" class="">
         <FormStepsCounter :currentStep="step" :totalSteps="6" />
         <FormSalutForm v-if="step === 0" @close="nextStep" @next="step++" />
